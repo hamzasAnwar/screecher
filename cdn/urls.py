d8f491c2b373383160cf9a689d4270e1e8b5242f
Ben Stock <stock@cispa.saarland>
2018-05-02 19:57:02 +0200

    fixing issue with CORS

--- cdn/urls.py
+++ cdn/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user/<int:profile_id>', views.user_info, name='user'),
+    path('user/', views.user_info, name='user'),
 ]
--- cdn/views.py
+++ cdn/views.py
@@ -3,12 +3,24 @@ from django.db import connections
 
 import json
 
+def is_allowed_origin(http_origin, http_host):
+    # allow www.teamX access
+    if http_origin:
+        # strip SSL
+        http_origin = http_origin.replace("https://", "")
+        if http_origin.startswith("www.team") and http_origin.replace("www.", "cdn.") == http_host:
+            return True
+    # allow access if proxy nullifies the header for privacy reasons
+    if http_origin == 'null':
+        return True
+    return False
 
-def user_info(request, profile_id):
-    if request.user.id != int(profile_id):
-        return HttpResponse("Wrong User ID: " + str(request.user.id))
+
+def user_info(request):
+    if request.user.id is None:
+        return HttpResponse("Sorry, not logged in.")
     cursor = connections['default'].cursor()
-    cursor.execute("SELECT first_name, last_name, username, email FROM screecher_userprofile WHERE user_id=%s", (profile_id,))
+    cursor.execute("SELECT first_name, last_name, username, email FROM screecher_userprofile WHERE user_id=%s", (request.user.id,))
     user_profile = cursor.fetchone()
     if user_profile is None:
         return HttpResponse('No such user ID!')
@@ -23,10 +35,7 @@ def user_info(request, profile_id):
     response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
     response['Access-Control-Allow-Headers'] = '*'
     response['Access-Control-Max-Age'] = 42
-    # Allow only certain domains and what might happen if proxies get in the way
-    parent_origin = '.'.join(request.META.get('HTTP_ORIGIN').split('.')[1:])
-    parent_host = '.'.join(request.META.get('HTTP_HOST').split('.')[1:])
-    if parent_origin in [parent_host, 'null']:
+    if is_allowed_origin(request.META.get('HTTP_ORIGIN'), request.META.get('HTTP_HOST')):
         response['Access-Control-Allow-Origin'] = request.META.get('HTTP_ORIGIN')
     response['Access-Control-Allow-Credentials'] = 'true'
     return response
